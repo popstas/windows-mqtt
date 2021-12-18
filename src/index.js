032c33d26cd1a9a1f369cf718878f168d5d176cf
Stanislav Popov <popstas@gmail.com>
2021-12-19 02:10:46 +0500

    refactor: main file decoupled to functions

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ if (isWindows) {
   windowsLogger = new EventLogger('windows-mqtt');
 }
 
-let modules;
-
 start();
 
 function log(msg, type = 'info') {
@@ -26,29 +24,57 @@ function log(msg, type = 'info') {
 
 async function start() {
   log('windows-mqtt started');
-  // get enabled modules from config
-  let modulesEnabled =  [];
+
+  const modulesEnabled = getModulesEnabled();
+
+  const modules = await initModules(modulesEnabled);
+
+  subscribeToModuleTopics(modules);
+
+  listenModulesMQTT(modules);
+}
+
+function listenModulesMQTT(modules) {
+  mqtt.on('message', async (topic, message) => {
+    const handler = getHandler(topic, modules);
+    if (!handler) {
+      log(`Cannot find handler for topic ${topic}`);
+      return;
+    }
+    // log(`< ${topic}: ${message}`);
+    handler(topic, message);
+  });
+}
+
+function getModulesEnabled() {
+  const modulesEnabled = [];
   for (let name in config.modules) {
     const mod = config.modules[name];
     const isEnabled = mod.enabled !== undefined ? !!mod.enabled : true;
-    if (isEnabled) modulesEnabled.push(name);
+    if (isEnabled)
+      modulesEnabled.push(name);
   }
+  return modulesEnabled;
+}
 
-  // init modules
-  modules = [];
+async function initModules(modulesEnabled) {
+  const modules = [];
   for (let name of modulesEnabled) {
     const opts = config.modules[name] || {};
     const mod = require('./modules/' + name);
 
     // default mqtt base
-    if (!opts.base) opts.base = `${config.mqtt.base}/${name}`
+    if (!opts.base)
+      opts.base = `${config.mqtt.base}/${name}`;
 
     log('load module: ' + name);
-    const modInited = await mod(mqtt, opts, log)
+    const modInited = await mod(mqtt, opts, log);
     modules.push(modInited);
   };
+  return modules;
+}
 
-  // subscribe all topics
+function subscribeToModuleTopics(modules) {
   let topics = [];
   for (let mod of modules) {
     const modTopics = mod.subscriptions.map(sub => Array.isArray(sub.topics) ? sub.topics : [sub.topics]).flat();
@@ -56,21 +82,9 @@ async function start() {
   }
   log(`\nSubscribe to topics:\n- ${topics.flat().join('\n- ')}\n`);
   mqtt.subscribe(topics.flat());
-
-  // on receive message
-  mqtt.on('message', async (topic, message) => {
-    const handler = getHandler(topic);
-    if (!handler) {
-      log(`Cannot find handler for topic ${topic}`);
-      return;
-    }
-    // log(`< ${topic}: ${message}`);
-
-    handler(topic, message);
-  });
 }
 
-function getHandler(topic) {
+function getHandler(topic, modules) {
   let handler;
   for (let mod of modules) {
     const sub = mod.subscriptions.find(sub => sub.topics.includes(topic))
